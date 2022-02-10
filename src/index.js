2ccb4e67485f3a875c10950d70104664a5115601
DESKTOP-5TQN32U\lipik <luka.vuk@datamir.info>
2022-02-10 11:50:38 +0100

    fixing security alerts

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
-import { robots } from './robots.js';
 
 
 ReactDOM.render(
   <React.StrictMode>
-      <App robots = {robots} />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
